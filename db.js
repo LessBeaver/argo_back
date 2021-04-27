e93c5180e4f1d8819818a7a046f16eea614917e6
Leslie <lesliec38@hotmail.fr>
2021-04-27 14:37:37 +0200

    add db.js file + update

--- db.js
+++ db.js
@@ -1,6 +1,18 @@
-const db = require('db')
-db.connect({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS
+require('dotenv').config();
+const mysql = require('mysql');
+
+const env = process.env;
+
+const connection = mysql.createConnection ({
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASS,
+    database: env.DB_NAME,
+    connectionLimit: 10,
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected!");
 });
--- server/index.js
+++ server/index.js
@@ -1,13 +1,98 @@
+require('dotenv').config();
 const express = require("express");
 
-const PORT = process.env.PORT || 3001
-
+const PORT = 3001
 const app = express();
 
-app.get("/api", (req,res) => {
-    res.json({ message: "Hello from server!"});
+const connection = require('../db');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const { body: formData } = req;
+    const query = 'INSERT INTO User SET ?';
+    connection.query(query, formData, (error, results) => {
+        try {
+            if (error) {
+                res.status(500).json({
+                    error: 'error',
+                    errorMessage: 'Our server encountered an error performing the request',
+                });
+            } else {
+                res.status(201).json({
+                    id: results.insertId,
+                    ...formData,
+                });
+            };
+        } catch(err) {
+            console.log(err)
+            res.status(500).json({
+                status: 'error',
+                errorMessage: 'Difficulté rencontrée lors de l\'ajout d\'un nouveau membre',
+            });
+        };
+    })
+    // try {
+    //     connection.query('INSERT INTO User SET ?', formData, (error, results) => {
+    //         if (error) {
+    //             res.status(500).json({
+    //                 error: 'error',
+    //                 errorMessage: 'Our server encountered an error performing the request',
+    //             });
+    //         } else {
+    //             res.status(201).json({
+    //                 id: results.insertId,
+    //                 ...formData,
+    //             });
+    //         };
+    //     });
+    // } catch(err) {
+    //     console.log(err)
+    //     res.status(500).json({
+    //         status: 'error',
+    //         errorMessage: 'Difficulté rencontrée lors de l\'ajout d\'un nouveau membre',
+    //     });
+    // };
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+app.get('/', (req, res) => {
+    try {
+        connection.query('SELECT id, name FROM User', (error, results) => {
+            if (error) {
+                res.status(500).json({
+                    error: 'error',
+                    errorMessage: 'Our server encountered an error performing the request',
+                });
+            } else {
+                res.status(200).json(results);
+                };
+            });
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({
+            status: 'error',
+            errorMessage: 'Difficulté rencontrée lors de la récupération des membres de l\'équipage',
+        });
+    };
 });
+
+app.get('/', (req, res) => {
+    connection.query('SELECT id, name FROM User', (error, results) => {
+        res.status(200).json(results);
+    });
+});
+
+// app.listen(PORT, () => {
+//     console.log(`Server listening on ${PORT}`);
+// });
+
+
+const server = app.listen(PORT, (err) => {
+    if (err) {
+      throw new Error(`An error occurred: ${err.message}`);
+    }
+    console.log(`🌍 Server is running on port ${PORT}`);
+  });
+  
+  module.exports = server;
